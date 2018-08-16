ee966be29a1f5e0ee7db94136ecfef502060b5e4
jizong <pengjizong@sjtu.edu.cn>
2018-08-16 15:47:00 -0400

    check the pipline of the fully supervised learning

--- full-main.py
+++ full-main.py
@@ -1,8 +1,7 @@
 # coding=utf8
-import copy
 import os
+
 import sys
-import pandas as pd
 
 sys.path.insert(-1, os.getcwd())
 import warnings
@@ -10,9 +9,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import matplotlib.pyplot as plt
-import numpy as np, pandas as pd
+import pandas as pd
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -22,13 +20,9 @@ from utils.enet import Enet
 
 from utils.utils import Colorize, dice_loss
 from torchnet.meter import AverageValueMeter
-from tqdm import tqdm
-from utils.visualize import Dashboard
 import click
 
-
 use_gpu = True
-# device = "cuda" if torch.cuda.is_available() and use_gpu else "cpu"
 device = torch.device('cuda') if torch.cuda.is_available() and use_gpu else torch.device('cpu')
 cuda_device = "0"
 os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
@@ -38,7 +32,6 @@ batch_size_val = 1
 num_workers = 7
 max_epoch = 100
 data_dir = 'dataset/ACDC-2D-All'
-
 
 color_transform = Colorize()
 transform = transforms.Compose([
@@ -52,7 +45,7 @@ mask_transform = transforms.Compose([
 
 train_set = medicalDataLoader.MedicalImageDataset('train', data_dir, transform=transform, mask_transform=mask_transform,
                                                   augment=True, equalize=False)
-train_loader = DataLoader(train_set,batch_size= batch_size, shuffle= True, num_workers= num_workers)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 val_set = medicalDataLoader.MedicalImageDataset('val', data_dir, transform=transform, mask_transform=mask_transform,
                                                 equalize=False)
@@ -64,7 +57,6 @@ train_iou_tables = []
 
 # train_broad=Dashboard(env='training')
 # val_broad=Dashboard(env='eval')
-
 
 
 def val(val_dataloader, network):
@@ -84,9 +76,7 @@ def val(val_dataloader, network):
             dice_meter_f.add(iou[1])
             dice_meter_b.add(iou[0])
 
-
     network.train()
-    # print('\nval iou:  %.6f' % dice_meter_f.value()[0])
     return [dice_meter_b.value()[0], dice_meter_f.value()[0]]
 
 
@@ -97,19 +87,19 @@ def main(lr):
     neural_net.to(device)
     criterion = CrossEntropyLoss2d()
     optimizer = torch.optim.Adam(params=neural_net.parameters(), lr=lr, weight_decay=1e-5)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=[20,40,60,80],gamma=0.25)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 40, 60, 80], gamma=0.25)
     highest_iou = -1
 
     plt.ion()
-    for epoch in tqdm(range(max_epoch)):
+    for epoch in range(max_epoch):
         scheduler.step()
         for param_group in optimizer.param_groups:
             _lr = param_group['lr']
-        for i, (img, full_mask,_, _) in tqdm(enumerate(train_loader)):
+        for i, (img, full_mask, _, _) in enumerate(train_loader):
             img, full_mask = img.to(device), full_mask.to(device)
             optimizer.zero_grad()
             output = neural_net(img)
-            loss = criterion(output,full_mask.squeeze(1))
+            loss = criterion(output, full_mask.squeeze(1))
             loss.backward()
             optimizer.step()
 
@@ -122,7 +112,7 @@ def main(lr):
         val_iou_tables.append(val_ious)
         print(
             '\n%d epoch: training fiou is: %.5f and val fiou is %.5f, with learning rate of %.6f' % (
-            epoch, train_ious[1], val_ious[1], _lr))
+                epoch, train_ious[2], val_ious[2], _lr))
 
         try:
             pd.DataFrame(train_iou_tables, columns=['learning rate', 'background', 'foregound']).to_csv(
@@ -136,9 +126,6 @@ def main(lr):
             print('The highest val fiou is %f' % val_ious[2])
             highest_iou = val_ious[2]
             torch.save(neural_net.state_dict(), 'checkpoint/pretrained_%.5f.pth' % val_ious[2])
-
-
-
 
 
 if __name__ == "__main__":
