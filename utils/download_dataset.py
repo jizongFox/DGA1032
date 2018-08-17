83baa5b1f16bc70e59a04866fcade2167770d40c
jizong <pengjizong@sjtu.edu.cn>
2018-08-17 15:57:16 -0400

    add mask skip mechanism

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -8,6 +8,6 @@ if not os.path.isdir(os.path.join(project_root_path, 'checkpoint')):
 if not os.path.isdir(os.path.join(project_root_path, 'results')):
     os.mkdir(os.path.join(project_root_path, 'results'))
 cmd_ = 'cp kaggle.json ~/.kaggle/ && chmod 600 ~/.kaggle/kaggle.json'
-
+os.system(cmd_)
 cmd = '"kaggle datasets download -d jizongpeng/acdc2dall --force -p %s"'%os.path.join(project_root_path,'dataset')
 os.system('/bin/zsh -c '+ cmd)
