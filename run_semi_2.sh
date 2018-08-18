f4de25268292869b7e377cc5399ea151071cd6d2
jizong <pengjizong@sjtu.edu.cn>
2018-08-18 19:29:30 -0400

    On graphcut baseline

--- run_semi_2.sh
+++ run_semi_2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=1 python semi-main.py --lamda 0.1  --saved_name lambda_0.1_graphcut_baseline
-CUDA_VISIBLE_DEVICES=1 python semi-main.py --lamda 5 --saved_name lambda_5_graphcut_baseline
+CUDA_VISIBLE_DEVICES=1 python semi-graphcut.py --lamda 0.1  --saved_name lambda_0.1_graphcut_baseline
+CUDA_VISIBLE_DEVICES=1 python semi-graphcut.py --lamda 5 --saved_name lambda_5_graphcut_baseline
 
