336fe62408493bd2fc0219baf40b8bd702e09272
jizong <pengjizong@sjtu.edu.cn>
2018-08-18 15:34:30 -0400

    working

--- run_semi_2.sh
+++ run_semi_2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=1 python semi-main.py
-CUDA_VISIBLE_DEVICES=1 python semi-main.py --lamda 0 --saved_name lambda_0
+CUDA_VISIBLE_DEVICES=1 python semi-main.py --lamda 0.1 --saved_name lambda_0.1
+CUDA_VISIBLE_DEVICES=1 python semi-main.py --lamda 5 --saved_name lambda_5
 
