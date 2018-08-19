ffe0dfd4f338500c24294327ffa6945f913bae49
jizong <pengjizong@sjtu.edu.cn>
2018-08-19 13:33:04 -0400

    weakly supervised setting on going

--- utils/download_dataset.py
+++
@@ -1,7 +0,0 @@
-import kaggle,os,subprocess
-
-project_root_path = os.path.dirname(os.getcwd())
-if not os.path.isdir(os.path.join(project_root_path,'dataset')):
-    os.mkdir(os.path.join(project_root_path,'dataset'))
-cmd = '"kaggle datasets download -d jizongpeng/acdc2dall --force -p %s"'%os.path.join(project_root_path,'dataset')
-os.system('/bin/zsh -c '+ cmd)
